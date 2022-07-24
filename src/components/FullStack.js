bb209689c1b5ef39ca7866304886c9bcdb79449a
unknown <artiomonilov2005@gmail.com>
2022-07-24 21:03:12 +0300

    FullStack Adapt New

--- src/components/FullStack.js
+++ src/components/FullStack.js
@@ -5,6 +5,8 @@ import useWindowSize from "../hooks/useWindowSize"
 function FullStack()
 {
     const [isPhoneSize] = useWindowSize(664, 0)
+    const [NoText] = useWindowSize(743, 0)//491px
+    const [WrapTitle] = useWindowSize(491, 0)
 
     return (
         <div style={{ backgroundColor: "#3E4E88" }}>
@@ -12,25 +14,28 @@ function FullStack()
                 style={{
                     backgroundColor: "white",
                 }}>
-                <div className="headline-sm text-center">All in one Developer ♛</div>
+                <div className="headline-sm text-center">All in one 
+                {WrapTitle && (<br />)} Developer ♛</div>
                 <div className="text-container text-center">
                     <div className="text-collapsed">
-                        The entire process goes through my hands.
+                        The entire process goes {isPhoneSize && (<br />)}
+                        through my hands.
                     </div>
                 </div>
-                <div className={isPhoneSize ? "contact-block" : "container-fullstack"}>
+
+                <div className="container-fullstack">
                     <section className="full-stack-container">
                         <img className="fullstack-img"
                             src="/images/pages/fullstack/1.gif"
                             alt="fullstack-1-img"
                         />
                         <div className="headline-xs">UI Design</div>
-                        <div className="text-container">
+                        {!NoText && (<div className="text-container">
                             <div className="text-collapsed-xs">
                                 The UI design sketch.
-                                Layout, Colors, Spacing, Style, Purpose.
+                                Layout, Style, Purpose.
                             </div>
-                        </div>
+                        </div>)}
                     </section>
                     <section className="full-stack-container">
                         <img className="fullstack-img"
@@ -38,12 +43,12 @@ function FullStack()
                             alt="fullstack-2-img"
                         />
                         <div className="headline-xs">Icons and Assets</div>
-                        <div className="text-container">
+                        {!NoText && (<div className="text-container">
                             <div className="text-collapsed-xs">
                                 Web assets and the entire brand.
                                 Icons, images, fonts.
                             </div>
-                        </div>
+                        </div>)}
                     </section>
                     <section className="full-stack-container">
                         <img className="fullstack-img"
@@ -51,27 +56,24 @@ function FullStack()
                             alt="fullstack-3-img"
                         />
                         <div className="headline-xs">Copywriting</div>
-                        <div className="text-container">
+                        {!NoText && (<div className="text-container">
                             <div className="text-collapsed-xs">
                                 Written content that influences the vistior.
                             </div>
-                        </div>
+                        </div>)}
                     </section>
-                </div>
-
-                <div className={isPhoneSize ? "contact-block" : "container-fullstack"}>
                     <section className="full-stack-container">
                         <img className="fullstack-img"
                             src="/images/pages/fullstack/4.gif"
                             alt="fullstack-4-img"
                         />
                         <div className="headline-xs">Client Development</div>
-                        <div className="text-container">
+                        {!NoText && (<div className="text-container">
                             <div className="text-collapsed-xs">
                                 Fully working client structure.
-                                The tool used by the user.
+                                Used by the user.
                             </div>
-                        </div>
+                        </div>)}
                     </section>
                     <section className="full-stack-container">
                         <img className="fullstack-img"
@@ -79,11 +81,12 @@ function FullStack()
                             alt="fullstack-5-img"
                         />
                         <div className="headline-xs">Server Development</div>
-                        <div className="text-container">
+                        {!NoText && (<div className="text-container">
                             <div className="text-collapsed-xs">
-                                Fully working server logic. Manages the user interaction.
+                                Fully working server logic. 
+                                Manages interaction.
                             </div>
-                        </div>
+                        </div>)}
                     </section>
                     <section className="full-stack-container">
                         <img className="fullstack-img"
@@ -91,14 +94,97 @@ function FullStack()
                             alt="fullstack-6-img"
                         />
                         <div className="headline-xs">Security</div>
-                        <div className="text-container">
+                        {!NoText && (<div className="text-container">
                             <div className="text-collapsed-xs">
                                 The base security to keep any application protected.
                             </div>
-                        </div>
+                        </div>)}
                     </section>
                 </div>
-            </section >
+
+
+                {/* <div className="container-fullstack">
+                    <section className="full-stack-container">
+                        <img className="fullstack-img"
+                            src="/images/pages/fullstack/1.gif"
+                            alt="fullstack-1-img"
+                        />
+                        <div className="headline-xs">UI Design</div>
+                        {!isPhoneSize && (<div className="text-container">
+                            <div className="text-collapsed-xs">
+                                The UI design sketch.
+                                Layout, Style, Purpose.
+                            </div>
+                        </div>)}
+                    </section>
+                    <section className="full-stack-container">
+                        <img className="fullstack-img"
+                            src="/images/pages/fullstack/2.gif"
+                            alt="fullstack-2-img"
+                        />
+                        <div className="headline-xs">Icons and Assets</div>
+                        {!isPhoneSize && (<div className="text-container">
+                            <div className="text-collapsed-xs">
+                                Web assets and the entire brand.
+                                Icons, images, fonts.
+                            </div>
+                        </div>)}
+                    </section>
+                    <section className="full-stack-container">
+                        <img className="fullstack-img"
+                            src="/images/pages/fullstack/3.gif"
+                            alt="fullstack-3-img"
+                        />
+                        <div className="headline-xs">Copywriting</div>
+                        {!isPhoneSize && (<div className="text-container">
+                            <div className="text-collapsed-xs">
+                                Written content that influences the vistior.
+                            </div>
+                        </div>)}
+                    </section>
+                </div>
+
+                <div className= "container-fullstack">
+                    <section className="full-stack-container">
+                        <img className="fullstack-img"
+                            src="/images/pages/fullstack/4.gif"
+                            alt="fullstack-4-img"
+                        />
+                        <div className="headline-xs">Client Development</div>
+                        {!isPhoneSize && (<div className="text-container">
+                            <div className="text-collapsed-xs">
+                                Fully working client structure.
+                                Used by the user.
+                            </div>
+                        </div>)}
+                    </section>
+                    <section className="full-stack-container">
+                        <img className="fullstack-img"
+                            src="/images/pages/fullstack/5.gif"
+                            alt="fullstack-5-img"
+                        />
+                        <div className="headline-xs">Server Development</div>
+                        {!isPhoneSize && (<div className="text-container">
+                            <div className="text-collapsed-xs">
+                                Fully working server logic. 
+                                Manages interaction.
+                            </div>
+                        </div>)}
+                    </section>
+                    <section className="full-stack-container">
+                        <img className="fullstack-img"
+                            src="/images/pages/fullstack/6.gif"
+                            alt="fullstack-6-img"
+                        />
+                        <div className="headline-xs">Security</div>
+                        {!isPhoneSize && (<div className="text-container">
+                            <div className="text-collapsed-xs">
+                                The base security to keep any application protected.
+                            </div>
+                        </div>)}
+                    </section>
+                </div> */}
+            </section>
         </div>
     )
 }
