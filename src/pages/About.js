315b3371365e8ea16919ba8ca36a3e7b26fbc6aa
unknown <artiomonilov2005@gmail.com>
2022-07-25 20:54:42 +0300

    About Page Bug

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,27 +10,29 @@ function About()
 
     const AboutContainer = () => (
         <section className={!Stack ? "container" : ""} style={Stack ? { width: "fit-content" } : {}}>
-            <div className="paragraph" >
-                <div className="headline-sm">About Me 👋</div>
-                <div className="text-container">
-                    <div className="text-collapsed text-about">
-                        <div>I am a web developer based in Moldova.</div>
-                        <div>Throughout my journey, I have worked for</div>
-                        <div>some customers localy, in multiple fields.</div>
-                    </div>
-                    <div className="text-collapsed text-about" style={{ marginTop: 40 }}>
-                        <div>I like to serve people and help out my</div>
-                        <div>customers as much as possible.</div>
-                    </div>
-                    <div className="text-collapsed text-about" style={{ marginTop: 40 }}>
-                        <div>I do care about the my clients and look</div>
-                        <div>further into contributing to this world.</div>
+            <Animate animation="step-anim" start>
+                <div className="paragraph" >
+                    <div className="headline-sm">About Me 👋</div>
+                    <div className="text-container">
+                        <div className="text-collapsed text-about">
+                            <div>I am a web developer based in Moldova.</div>
+                            <div>Throughout my journey, I have worked for</div>
+                            <div>some customers localy, in multiple fields.</div>
+                        </div>
+                        <div className="text-collapsed text-about" style={{ marginTop: 40 }}>
+                            <div>I like to serve people and help out my</div>
+                            <div>customers as much as possible.</div>
+                        </div>
+                        <div className="text-collapsed text-about" style={{ marginTop: 40 }}>
+                            <div>I do care about the my clients and look</div>
+                            <div>further into contributing to this world.</div>
+                        </div>
                     </div>
                 </div>
-            </div>
-            {!Stack && (<Animate animation="opacity-anim">
-                <img src="/images/github.png" alt="profile-intro-img" className="profile" />
-            </Animate>)}
+                {!Stack && (<Animate animation="opacity-anim">
+                    <img src="/images/github.png" alt="profile-intro-img" className="profile" />
+                </Animate>)}
+            </Animate>
         </section>)
 
     const SkillContainer = () => (
@@ -38,105 +40,107 @@ function About()
             className="margin-total-sm margin-total-about"
             style={{ width: "100%", width: "fit-content" }}
         >
-            <div className="headline-sm">Relevant Skills</div>
-            <div className="skill-row" >
-                <div className="skill-container">
-                    <img
-                        className="skill-img"
-                        src="/images/skills/html.png"
-                        alt="html-skill"
-                    />
-                </div>
-                <div className="skill-container">
-                    <img
-                        className="skill-img"
-                        src="/images/skills/css.png"
-                        alt="css-skill"
-                    />
-                </div>
-                <div className="skill-container">
-                    <img
-                        className="skill-img"
-                        src="/images/skills/js.png"
-                        alt="js-skill"
-                    />
-                </div>
-                {/* <div className="skill-container">
+            <Animate animation="step-anim" start>
+                <div className="headline-sm">Relevant Skills</div>
+                <div className="skill-row" >
+                    <div className="skill-container">
+                        <img
+                            className="skill-img"
+                            src="/images/skills/html.png"
+                            alt="html-skill"
+                        />
+                    </div>
+                    <div className="skill-container">
+                        <img
+                            className="skill-img"
+                            src="/images/skills/css.png"
+                            alt="css-skill"
+                        />
+                    </div>
+                    <div className="skill-container">
+                        <img
+                            className="skill-img"
+                            src="/images/skills/js.png"
+                            alt="js-skill"
+                        />
+                    </div>
+                    {/* <div className="skill-container">
                         <img
                             className="skill-img"
                             src="/images/skills/csharp.png"
                             alt="csharp-skill"
                         />
                     </div> */}
-                <a className="skill-container" href="https://www.mongodb.com/">
-                    <img
-                        className="skill-img"
-                        src="/images/skills/mongodb.png"
-                        alt="mongodb-skill"
-                    />
-                </a>
-                <a className="skill-container" href="https://nodejs.org/">
-                    <img
-                        className="skill-img"
-                        src="/images/skills/nodejs.png"
-                        alt="nodejs-skill"
-                    />
-                </a>
-            </div>
-            <div className="skill-row">
-                <a className="skill-container" href="https://reactjs.org/">
-                    <img
-                        className="skill-img"
-                        src="/images/skills/react.png"
-                        alt="react-skill"
-                    />
-                </a>
-                <a className="skill-container" href="https://nextjs.org/">
-                    <img
-                        className="skill-img"
-                        src="/images/skills/nextjs.png"
-                        alt="nextjs-skill"
-                    />
-                </a>
-                <a className="skill-container" href="https://expressjs.com/">
-                    <img
-                        style={{ height: 23 }}
-                        className="skill-img"
-                        src="/images/skills/express.png"
-                        alt="express-skill"
-                    />
-                </a>
-                <a className="skill-container" href="https://git-scm.com/">
-                    <img
-                        className="skill-img"
-                        src="/images/skills/git.png"
-                        alt="git-skill"
-                    />
-                </a>
-                <a className="skill-container" href="https://github.com/">
-                    <img
-                        className="skill-img"
-                        src="/images/github.png"
-                        alt="github-skill"
-                    />
-                </a>
-            </div>
-            <div className="skill-row">
-                <a className="skill-container" href="https://www.adobe.com/products/photoshop.html">
-                    <img
-                        className="skill-img"
-                        src="/images/skills/photoshop.png"
-                        alt="photoshop-skill"
-                    />
-                </a>
-                <a className="skill-container" href="https://www.blender.org/">
-                    <img
-                        className="skill-img"
-                        src="/images/skills/blender.png"
-                        alt="blender-skill"
-                    />
-                </a>
-            </div>
+                    <a className="skill-container" href="https://www.mongodb.com/">
+                        <img
+                            className="skill-img"
+                            src="/images/skills/mongodb.png"
+                            alt="mongodb-skill"
+                        />
+                    </a>
+                    <a className="skill-container" href="https://nodejs.org/">
+                        <img
+                            className="skill-img"
+                            src="/images/skills/nodejs.png"
+                            alt="nodejs-skill"
+                        />
+                    </a>
+                </div>
+                <div className="skill-row">
+                    <a className="skill-container" href="https://reactjs.org/">
+                        <img
+                            className="skill-img"
+                            src="/images/skills/react.png"
+                            alt="react-skill"
+                        />
+                    </a>
+                    <a className="skill-container" href="https://nextjs.org/">
+                        <img
+                            className="skill-img"
+                            src="/images/skills/nextjs.png"
+                            alt="nextjs-skill"
+                        />
+                    </a>
+                    <a className="skill-container" href="https://expressjs.com/">
+                        <img
+                            style={{ height: 23 }}
+                            className="skill-img"
+                            src="/images/skills/express.png"
+                            alt="express-skill"
+                        />
+                    </a>
+                    <a className="skill-container" href="https://git-scm.com/">
+                        <img
+                            className="skill-img"
+                            src="/images/skills/git.png"
+                            alt="git-skill"
+                        />
+                    </a>
+                    <a className="skill-container" href="https://github.com/">
+                        <img
+                            className="skill-img"
+                            src="/images/github.png"
+                            alt="github-skill"
+                        />
+                    </a>
+                </div>
+                <div className="skill-row">
+                    <a className="skill-container" href="https://www.adobe.com/products/photoshop.html">
+                        <img
+                            className="skill-img"
+                            src="/images/skills/photoshop.png"
+                            alt="photoshop-skill"
+                        />
+                    </a>
+                    <a className="skill-container" href="https://www.blender.org/">
+                        <img
+                            className="skill-img"
+                            src="/images/skills/blender.png"
+                            alt="blender-skill"
+                        />
+                    </a>
+                </div>
+            </Animate>
         </section>
     )
 
@@ -159,23 +163,16 @@ function About()
                 {!Stack || Small
                     ? (
                         <div style={{ width: "100%" }} className="center-container">
-                            <Animate animation="step-anim" start>
-                                <AboutContainer />
-                                <SkillContainer />
-                            </Animate>
+                            <AboutContainer />
+                            <SkillContainer />
                         </div>)
                     : (
                         <>
                             <div style={{ width: "100%" }} className="center-container">
-                                <Animate animation="step-anim" start>
-                                    <AboutContainer />
-                                </Animate>
+                                <AboutContainer />
                             </div>
-                            <Animate animation="step-anim" start>
-                                <SkillContainer />
-                            </Animate>
+                            <SkillContainer />
                         </>)}
-
             </div>
         </div>
     )
