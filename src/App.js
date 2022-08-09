25a2fa4a3305a8d734f9a33c4621265fa4d7700c
unknown <artiomonilov2005@gmail.com>
2022-08-09 20:36:41 +0300

    CSR Loading

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react"
 import { Routes, Route } from "react-router-dom";
 import Home from './pages/Home'
 import Portofolio from "./pages/Portofolio";
@@ -7,6 +8,19 @@ import ScrollListen from "./components/ScrollListen";
 
 function App()
 {
+	const [loading, setLoading] = useState(true)
+
+	useEffect(() =>
+	{
+		// demoAsyncCall().then(() => setLoading(false))
+		setLoading(false)
+	}, [])
+
+	if (loading)
+	{
+		return null;
+	}
+
 	return (
 		<Layout>
 			<ScrollListen />
@@ -17,6 +31,11 @@ function App()
 			</Routes>
 		</Layout>
 	);
+}
+
+function demoAsyncCall()
+{
+	return new Promise((resolve) => setTimeout(() => resolve(), 2500));
 }
 
 export default App;
